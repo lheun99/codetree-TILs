53b9f9ff8878d57d1cf06184bf2a2afa972d1b84
Lee Ha-eun <97579695+lheun99@users.noreply.github.com>
2024-07-30 10:17:49 +0900

    Title: 1까지 나누기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/1까지 나누기/divide-by-1.py
+++ 240730/1까지 나누기/divide-by-1.py
@@ -1,6 +1,11 @@
 a = int(input())
-i = 0
-while a % i <= 1:
-    a //= i 
-    i += 1
-print(i)
+
+res = 1
+for i in range(1, a+1):
+    if a // i <= 1:
+        break
+    else:
+        a //= i
+        res += 1
+
+print(res)

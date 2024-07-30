044fe928b1e2c3a5b621a1392b0e8a0da51f1282
Lee Ha-eun <97579695+lheun99@users.noreply.github.com>
2024-07-30 10:06:24 +0900

    Title: 조건에 부합하는 수, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240730/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -1,7 +1,7 @@
 a = int(input())
 
 for i  in range(1, a+1):
-    if (i%2 != 0 and i%4 == 0) and ((i//8) % 2 != 0) and (i%7 >= 4):
+    if (i%2 != 0 or i%4 == 0) and ((i//8) % 2 != 0) and (i%7 >= 4):
+        print(i, end = " ")
+    else:
         continue
-
-    print(i)

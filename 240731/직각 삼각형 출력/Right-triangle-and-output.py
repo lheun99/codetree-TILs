e666614ca751755836e601141e05078115972f32
Lee Ha-eun <97579695+lheun99@users.noreply.github.com>
2024-07-31 09:31:27 +0900

    Title: 직각 삼각형 출력, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240731/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,10 +1,5 @@
 n = int(input())
 
-print("*")
-for i in range(1, n):
-    if n % 2 == 0:
-        a = i * 2
-    else:
-        a = i * 2 + 1
-
+for i in range(1, n+1):
+    a = i * 2 - 1
     print("*" * a)

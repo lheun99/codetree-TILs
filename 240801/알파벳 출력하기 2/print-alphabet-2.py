57e81bb8f5ba83beb037f2a2b98c28349a509e97
Lee Ha-eun <97579695+lheun99@users.noreply.github.com>
2024-08-01 10:20:21 +0900

    Title: 알파벳 출력하기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240801/알파벳 출력하기 2/print-alphabet-2.py
+++ 240801/알파벳 출력하기 2/print-alphabet-2.py
@@ -2,8 +2,11 @@ n = int(input())
 num = ord('A')
 
 for i in range(n):
-    for j in range(n):
-        print(" " * j, end = " ")
-
-    for z in range(n, 0, -1):
-        print(z, end=" ")
+    print("  " * i, end = "")
+    
+    for j in range(n, 0, -1):
+        print(chr(num), end = " ")
+        #print(chr(num), end = " ")
+        num += 1
+    n -= 1
+    print()

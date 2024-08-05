3558f296fa2fae389d032ff15024df2a5173b6cd
Lee Ha-eun <97579695+lheun99@users.noreply.github.com>
2024-08-05 12:30:26 +0900

    Title: 미는 횟수, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240805/미는 횟수/number-of-pushes.py
+++ 240805/미는 횟수/number-of-pushes.py
@@ -3,11 +3,12 @@ b = input()
 
 cnt = 1
 for i in range(len(a)-1):
-    a = a[1:]+a[0]
+    a = a[-1]+a[0:-1]
 
     if a == b:
         print(cnt)
+        break
     cnt += 1
 
-if cnt == len(a):
+if cnt == len(b):
     print(-1)
